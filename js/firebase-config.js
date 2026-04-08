45453baf39832e9d1d611f6a1745a4e70b6071d2
Paul <paulbonilla2005@gmail.com>
2026-04-08 10:31:06 -0500

    git ignore

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -2,7 +2,12 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/fireba
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 const firebaseConfig = {
-
+  apiKey: "TAIzaSyA6MwI4jb3WCB7zS22drrQVd9F045g-sac",
+  authDomain: "riocontainer.firebaseapp.com",
+  projectId: "riocontainer",
+  storageBucket: "riocontainer.firebasestorage.app",
+  messagingSenderId: "124480610032",
+  appId: "1:124480610032:web:211347945a005a2920ec35"
 };
 
 const app = initializeApp(firebaseConfig);
